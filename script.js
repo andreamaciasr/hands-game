b9fb66ba2c87cc66b0f35390ca6b1041b1747de4
Andrea Macias <andreamacrz@gmail.com>
2024-08-05 18:23:30 -0400

    added images wip

--- script.js
+++ script.js
@@ -21,7 +21,8 @@ document.addEventListener("keydown", (event) =>
 /*----- functions -----*/
 
 function checkKey(event) {
-  let key = event.key;
+  let key = event.key.toLowerCase();
+  changeImage(key);
   if (currentSequence.length == 0) {
     iteration++;
     currentSequence = getSequence(iteration);
@@ -35,6 +36,45 @@ function checkKey(event) {
   } else {
     banner.innerText = "fail";
     return false;
+  }
+}
+
+function changeImage(key) {
+  switch (key) {
+    case "d":
+      document.getElementById("d").src = "./images/d2.png";
+      document.getElementById("g").src = "./images/g1.jpg";
+      document.getElementById("h").src = "./images/h1.png";
+      document.getElementById("k").src = "./images/k1.png";
+      break;
+    case "g":
+      document.getElementById("d").src = "./images/d1.png";
+      document.getElementById("g").src = "./images/g2.jpg";
+      document.getElementById("h").src = "./images/h1.png";
+      document.getElementById("k").src = "./images/k1.png";
+      document.getElementById("spacebar").src = "./images/spacebar1.png";
+      break;
+    case "h":
+      document.getElementById("d").src = "./images/d1.png";
+      document.getElementById("g").src = "./images/g1.jpg";
+      document.getElementById("h").src = "./images/h2.png";
+      document.getElementById("k").src = "./images/k1.png";
+      document.getElementById("spacebar").src = "./images/spacebar1.png";
+      break;
+    case "k":
+      document.getElementById("d").src = "./images/d1.png";
+      document.getElementById("g").src = "./images/g1.jpg";
+      document.getElementById("h").src = "./images/h1.png";
+      document.getElementById("k").src = "./images/k2.png";
+      document.getElementById("spacebar").src = "./images/spacebar1.png";
+      break;
+    case " ":
+      document.getElementById("d").src = "./images/d1.png";
+      document.getElementById("g").src = "./images/g1.jpg";
+      document.getElementById("h").src = "./images/h1.png";
+      document.getElementById("k").src = "./images/k1.png";
+      document.getElementById("spacebar").src = "./images/spacebar2.png";
+      break;
   }
 }
 
